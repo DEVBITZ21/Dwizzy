11b878351a32aade53d6c7679832f4424f40dd4b
lnathilina <thilina.aravinda1999@gmail.com>
2021-09-27 19:50:05 +0530

    Have fix some

--- app/src/main/java/Database/Database.java
+++ app/src/main/java/Database/Database.java
@@ -33,13 +33,13 @@ public class Database extends SQLiteAssetHelper {
         if (c.moveToFirst())
         {
             do{
-                result.add( new Order( c.getString( c.getColumnIndex( "ProductId" ) ) )
-
-                        c.getString(c.getColumnIndex( "ProductName"  ) ),
+                result.add( new Order(
+                        c.getString( c.getColumnIndex( "ProductId" ) ) ,
+                        c.getString(c.getColumnIndex( "ProductName"  )),
                         c.getString(c.getColumnIndex( "Quantity" )),
                         c.getString(c.getColumnIndex( "Price" )) ,
                         c.getString(c.getColumnIndex("Discount"))
-                            );
+                            ));
             }while (c.moveToNext());
         }
         return result;
